0a93d644d6788176b5513cf393753b4dc6c30a86
Paul <paulbyrne99@rocketmail.com>
2020-08-23 16:27:47 +0100

    Basic cv html

--- mysite/CV/tests.py
+++ mysite/CV/tests.py
@@ -1,3 +1,19 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from CV.views import cv_page  
 
-# Create your tests here.
+class PageTest(TestCase):
+
+    #def test_url_resolves_to_page_view(self):
+     #   found = resolve('/cv')  
+      #  self.assertEqual(found.func, cv_page) 
+    
+
+    def test_page_returns_correct_html(self):
+        request = HttpRequest()  
+        response = cv_page(request)  
+        html = response.content.decode('utf8')  
+        self.assertTrue(html.startswith('<html>'))  
+        self.assertIn('<title>My CV</title>', html)  
+        self.assertTrue(html.endswith('</html>'))  
--- mysite/CV/views.py
+++ mysite/CV/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+# The CV page
+def cv_page(request):
+    return HttpResponse('<html><title>My CV</title></html>')
