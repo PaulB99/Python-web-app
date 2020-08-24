b32323b1b5578b903a9155b6ed4c942c7979520f
Paul <paulbyrne99@rocketmail.com>
2020-08-24 14:31:07 +0100

    basic cv page created

--- mysite/CV/views.py
+++ mysite/CV/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 # Create your views here.
 
 # The CV page
+
 def cv_page(request):
-    return HttpResponse('<html><title>My CV</title></html>')
+    return render(request, 'cv_base.html')
